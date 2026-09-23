87c4e459382fdc5cd30e895d57f7f77f2c7fa672
okellokolo <okellokolo@gmail.com>
2026-09-22 22:44:02 -0700

    completed word translation

---
+++ js/02-word-translator.js
@@ -0,0 +1,13 @@
+// Enter the language code
+let languageCode = prompt("Enter a language code (es, de, en, fr):");
+
+// A simple if statement
+if (languageCode === "es") {
+  document.write("Hola Mundo");
+} else if (languageCode === "de") {
+  cdocument.write("Hallo Welt");
+} else if (languageCode === "fr") {
+  document.write("Bonjour le monde");
+} else {
+  document.write("Hello World"); //default to English
+}
